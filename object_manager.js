e0edb8e01db79c271b5330ae2f6e5dabfd7c8ef1
tasabina <32531721+tasabina@users.noreply.github.com>
2018-08-03 16:14:58 +1200

    Update object_manager.js

--- object_manager.js
+++ object_manager.js
@@ -1,117 +1,90 @@
+//Create a map object on the site
 ymaps.ready(init);
-
     function init () {
+//Map properties
         var myMap = new ymaps.Map('map', {
-            center: [00,00],
-            zoom: 7,
-			behaviors: ['disable.scrollZoom', 'drag'],
-			controls: ['searchControl','zoomControl','rulerControl'],},
-			{maxZoom:11,minZoom:6});
-		
+            	center: [00,00],
+            	zoom: 7,
+		behaviors: ['disable.scrollZoom', 'drag'],
+		controls: ['searchControl','zoomControl','rulerControl'],},
+		{maxZoom:11,minZoom:6});
+//Add objects to the map that appear.Set their properties. The blue placemarks.		
     $.getJSON( "data.json", function(data){
-  
 	    var obj = ymaps.geoQuery(data)
 	    .setOptions('preset', 'islands#darkBlueCircleIcon')
 	    .addToMap(myMap)});
-
+//If we don't want the outline was visible, ask the appropriate option
+// Describe the options of the geoobject.Fill color, opacity, stroke color, width, style. Add an object to the map.	    
     function onPolygonLoad (json) {
         regionPolygon = new ymaps.Polygon(json.coordinates);
-        // Если мы не хотим, чтобы контур был виден, зададим соответствующую опцию.
         regionPolygon.options.set({
-        // Описываем опции геообъекта.
-        // Цвет заливки.
         fillColor: '#0099FF',
-        // Цвет обводки.
         strokeColor: '#0066CC',
-        // Общая прозрачность (как для заливки, так и для обводки).
         fillOpacity: 0.3,
-        // Ширина обводки.
         strokeWidth: 2,
-        // Стиль обводки.
         strokeStyle: 'solid'
     });
-        // Чтобы корректно осуществлялись геометрические операции
-        // над спроецированным многоугольником, его нужно добавить на карту.
-        myMap.geoObjects.add(regionPolygon);
-
+        myMap.geoObjects.add(regionPolygon);	    
+//Add objects to the map that appear on the condition.Set their properties. The red placemarks.
     $.getJSON( "data.json", function(data){
-
-        var objects = ymaps.geoQuery(data)
-        .addToMap(myMap)
-        var objectsInsidepolygon = objects.searchInside(regionPolygon);
-            objectsInsidepolygon.setOptions('preset', 'islands#redIcon');
-            // Оставшиеся объекты - синими.
-            objects.remove(objectsInsidepolygon).setOptions('preset', 'islands#darkBlueCircleDotIcon')
-
-        });
-
-        }
+		var objects = ymaps.geoQuery(data)
+		.addToMap(myMap)
+		var objectsInsidepolygon = objects.searchInside(regionPolygon);
+		objectsInsidepolygon.setOptions('preset', 'islands#redIcon');
+		objects.remove(objectsInsidepolygon).setOptions('preset', 'islands#darkBlueCircleDotIcon')
+        	});
+        }	    
+//Clustering objects. Disabled by default.	    
         objectManager = new ymaps.ObjectManager({
-                // Чтобы метки начали кластеризоваться, выставляем опцию.
-        clusterize: false,
-                // ObjectManager принимает те же опции, что и кластеризатор.
-        gridSize: 512,
-        minClusterSize : 5,
-            });
-
-        // Чтобы задать опции одиночным объектам и кластерам,
-        // обратимся к дочерним коллекциям ObjectManager.
+		clusterize: false,
+		gridSize: 512,
+		minClusterSize : 5,
+            });	    
+//Set clustering objects properties. 
         objectManager.objects.options.set({iconLayout: 'default#image', iconImageHref: 'icon_1.png',iconImageOffset: [-12, -50],iconImageSize: [25, 50],hasHint: true});
         objectManager.clusters.options.set('preset', 'islands#invertedDarkBlueClusterIcons');
-        myMap.geoObjects.add(objectManager);	 
-
+        myMap.geoObjects.add(objectManager);	    
+// Add polygons to the map asynchronously
         $.ajax({
             url: "data.json"
         }).done(function(data) {
             objectManager.add(data);
-        });		 
-
+        });	    
+// This item will serve as a container for the list items.
+// Depending on whether the list is collapsed or expanded, this container will hide or appear together with their children. API Yandex.maps    
        	ListBoxLayout = ymaps.templateLayoutFactory.createClass(
-        "<button id='my-listbox-header' class='btn btn-success dropdown-toggle' data-toggle='dropdown'>" +
-        "{{data.title}} <span class='caret'></span>" +
-        "</button>" +
-        // Этот элемент будет служить контейнером для элементов списка.
-        // В зависимости от того, свернут или развернут список, этот контейнер будет
-        // скрываться или показываться вместе с дочерними элементами.
-        "<ul id='my-listbox'" +
-        " class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu'" +
-        " style='display: {% if state.expanded %}block{% else %}none{% endif %};'></ul>", {
-	build: function() {
-	// Вызываем метод build родительского класса перед выполнением
-	// дополнительных действий.
-	ListBoxLayout.superclass.build.call(this);
-	this.childContainerElement = $('#my-listbox').get(0);
-	// Генерируем специальное событие, оповещающее элемент управления
-	// о смене контейнера дочерних элементов.
-	this.events.fire('childcontainerchange', {
-	newChildContainerElement: this.childContainerElement,
-	oldChildContainerElement: null
-		});
-	},
-	// Переопределяем интерфейсный метод, возвращающий ссылку на
-	// контейнер дочерних элементов.
+		"<button id='my-listbox-header' class='btn btn-success dropdown-toggle' data-toggle='dropdown'>" +
+		"{{data.title}} <span class='caret'></span>" +
+		"</button>" +
+		"<ul id='my-listbox'" +
+		" class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu'" +
+		" style='display: {% if state.expanded %}block{% else %}none{% endif %};'></ul>", {
+			build: function() {
+				ListBoxLayout.superclass.build.call(this);
+				this.childContainerElement = $('#my-listbox').get(0);
+				this.events.fire('childcontainerchange', {
+					newChildContainerElement: this.childContainerElement,
+					oldChildContainerElement: null
+					});
+		},			
+// Override the interface method that returns a reference to the child container.
 	getChildContainerElement: function () {
-	return this.childContainerElement;
-	}, clear: function () {
-	// Заставим элемент управления перед очисткой макета
-	// откреплять дочерние элементы от родительского.
-	// Это защитит нас от неожиданных ошибок,
-	// связанных с уничтожением dom-элементов в ранних версиях ie.
+		return this.childContainerElement;
+	}, clear: function () {		
+// Let's force the control to detach the child elements from the parent before clearing the layout. Yandex.API Recommendation
 	this.events.fire('childcontainerchange', {
-	newChildContainerElement: null,
-	oldChildContainerElement: this.childContainerElement
+		newChildContainerElement: null,
+		oldChildContainerElement: this.childContainerElement
 	});
-	this.childContainerElement = null;
-	// Вызываем метод clear родительского класса после выполнения
-	// дополнительных действий.
-	ListBoxLayout.superclass.clear.call(this);
+		this.childContainerElement = null;
+		ListBoxLayout.superclass.clear.call(this);
 		}
-	}),
-	// Также создадим макет для отдельного элемента списка.
+	}),		
+// Also create a layout for an individual list item.
 	ListBoxItemLayout = ymaps.templateLayoutFactory.createClass(
 	"<li id='my-dropbox'><a>{{data.content}}</a></li>"
-	),
-	// Создадим пункты выпадающего списка
+	),		
+// Create drop-down list items and links to objects
             listBoxItems = [
                 new ymaps.control.ListBoxItem({
                     data: {
@@ -134,28 +107,22 @@ ymaps.ready(init);
                         zoom: 9
                     }
                 })
-
-            ],
-
-            // Теперь создадим список, содержащий 2 пунтка.
+            ],	
+// Set list properties.		
             listBox = new ymaps.control.ListBox({
                     items: listBoxItems,
                     data: {
                         title: '***'
                     },
                     options: {
-                        // С помощью опций можно задать как макет непосредственно для списка,
                         layout: ListBoxLayout,
-                        // так и макет для дочерних элементов списка. Для задания опций дочерних
-                        // элементов через родительский элемент необходимо добавлять префикс
-                        // 'item' к названиям опций.
                         itemLayout: ListBoxItemLayout
                     }
-                });
-    function lp (item) {
+                });	    
+// Builds geometric shapes on the map by coordinates by reference to the object	    
+    function regbuild (item) {
         switch(item.data.get('id')){
            case 1:
-
                 $.ajax({
                 url: 'region/region1.json',
                 dataType: 'json',
@@ -164,7 +131,6 @@ ymaps.ready(init);
             myMap.geoObjects.removeAll();
            break;
            case 2:
-
                 $.ajax({
                 url: 'region/region2.json',
                 dataType: 'json',
@@ -174,21 +140,19 @@ ymaps.ready(init);
            break;
            default:
               alert('!!!');
-        }}
+        }}	    
+// Get a link to the object you clicked on.
+// List item events prepariruetsya and they can listen to the parent element.	    	    
      listBox.events.add('click', function (e) {
-         // Получаем ссылку на объект, по которому кликнули.
-         // События элементов списка пропагируются
-         // и их можно слушать на родительском элементе.
          var item = e.get('target');
-         // Клик на заголовке выпадающего списка обрабатывать не надо.
          if (item != listBox) {
              myMap.setCenter(
                  item.data.get('center'),
                  item.data.get('zoom'),
-                 lp(item)
+                 regbuild(item)
                 );
             }
-        });
-
+        });	    
+// Adds a list to the map
     myMap.controls.add(listBox, {float: 'right'});	
 }
